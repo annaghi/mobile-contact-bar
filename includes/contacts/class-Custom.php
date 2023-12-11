1ef9e0928fc3187d569da82d68cc70afa7b6413b
Anna Bansaghi <anna.bansaghi@gmail.com>
2023-12-11 19:32:40 +0100

    Whitespace

--- includes/contacts/class-Custom.php
+++ includes/contacts/class-Custom.php
@@ -6,42 +6,42 @@ defined( 'ABSPATH' ) or exit;
 final class Mobile_Contact_Bar_Contact_Custom
 {
 
-	private static $type = null;
+    private static $type = null;
 
-	public static function plugins_loaded()
-	{
-		self::$type = substr( __CLASS__, 27 );
+    public static function plugins_loaded()
+    {
+        self::$type = substr( __CLASS__, 27 );
 
-		if( is_admin() )
-		{
-			add_filter( 'mcb_admin_add_button'  , array( __CLASS__, 'mcb_admin_add_button' ));
-			add_filter( 'mcb_admin_add_contact' , array( __CLASS__, 'mcb_admin_add_contact' ));
-		}
-	}
-
-
-
-	public static function mcb_admin_add_button( $buttons )
-	{
-		$buttons[5] = array(
-			'type'  => self::$type,
-			'title' => __( '+ New Button', 'mobile-contact-bar' ),
-		);
-		return $buttons;
-	}
+        if( is_admin() )
+        {
+            add_filter( 'mcb_admin_add_button'  , array( __CLASS__, 'mcb_admin_add_button' ));
+            add_filter( 'mcb_admin_add_contact' , array( __CLASS__, 'mcb_admin_add_contact' ));
+        }
+    }
 
 
 
-	public static function mcb_admin_add_contact( $contacts )
-	{
-		$contacts[] = array(
-			'checked'     => 0,
-			'type'        => self::$type,
-			'icon'        => '',
-			'title'       => '',
-			'placeholder' => 'URI',
-			'uri'         => '',
-		);
-		return $contacts;
-	}
+    public static function mcb_admin_add_button( $buttons )
+    {
+        $buttons[5] = array(
+            'type'  => self::$type,
+            'title' => __( '+ New Button', 'mobile-contact-bar' ),
+        );
+        return $buttons;
+    }
+
+
+
+    public static function mcb_admin_add_contact( $contacts )
+    {
+        $contacts[] = array(
+            'checked'     => 0,
+            'type'        => self::$type,
+            'icon'        => '',
+            'title'       => '',
+            'placeholder' => 'URI',
+            'uri'         => '',
+        );
+        return $contacts;
+    }
 }
